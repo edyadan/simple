03e11720ba2203c6d5715e964bc8aa588808d073
edyadan <edyadan@outlook.com>
2019-08-16 16:13:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
-/*
-Copyright 2017 Google Inc.
+$(document).ready(
+     (function () {
+         'use strict';
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    var constraints = {
+        video: {
+            width: 400,
+            height: 300,
+            facingMode: "environment"
+        }
+    };
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    var video = document.querySelector('video');
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-'use strict';
-
-var constraints = {
-    video: {
-        width: 400,
-        height: 300,
-        facingMode: "environment"
+    function handleSuccess(stream) {
+      window.stream = stream; // only to make stream available to console
+      video.srcObject = stream;
     }
-};
 
-var video = document.querySelector('video');
+    function handleError(error) {
+      console.log('getUserMedia error: ', error);
+    }
 
-function handleSuccess(stream) {
-  window.stream = stream; // only to make stream available to console
-  video.srcObject = stream;
-}
+    navigator.mediaDevices.getUserMedia(constraints).
+      then(handleSuccess).catch(handleError);
+    var video = document.querySelector('video');
+    const canvas = document.querySelector('#canvas-element');
+    var i = 0;
+    <!--setInterval(explode, 100); -->
+    $( "#add" ).click(function() {
+       canvas.width = video.videoWidth;
+       canvas.height = video.videoHeight;
+       canvas.getContext('2d').drawImage(video, 0, 0);
+       var str =  $( "#imgInp" ).val();
+       var newElement2 = '<tr><td><div><img id="screenshot-img'+i+'" src=""></div><input name="imgInp'+i+'" type="text" id="imgInp'+i+'" value="'+str+'"/></td></tr>';
+       $( "#mytable" ).append( $(newElement2) );
+       const img = document.querySelector('#screenshot-img'+i);
+       img.src = canvas.toDataURL('image/png');
+       i += 1;
+    }); 
+    $( "#send" ).click(function() {
+	   const img1 = document.querySelector('#screenshot-img0');
+       const show1 = document.querySelector('#show-img1');
+       show1.src=img1.src;
+       console.log(img1.src)
+    });
+    function explode(){
+       canvas.width = video.videoWidth;
+       canvas.height = video.videoHeight;
+       canvas.getContext('2d').drawImage(video, 0, 0);
+      const show1 = document.querySelector('#show-img1');
+      show1.src = canvas.toDataURL('image/png');
+    };   
+	})()
+);
 
-function handleError(error) {
-  console.log('getUserMedia error: ', error);
-}
-
-navigator.mediaDevices.getUserMedia(constraints).
-  then(handleSuccess).catch(handleError);
